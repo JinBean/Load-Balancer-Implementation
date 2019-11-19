f132824739850b3150d64909b6da50f510e03991
jinbean <weijin_tan@mymail.sutd.edu.sg>
2019-11-19 14:42:23 +0800

    Add comments

--- nginx_implementation.py
+++ nginx_implementation.py
@@ -9,8 +9,9 @@ import requests
 import random
 import config
 
-multiprocessing.set_start_method('spawn', True)
+multiprocessing.set_start_method('spawn', True) # Necessary for multiprocessing library to work on linux
 
+# All the actual work happens in the worker function
 def worker(socket, worker_num):
   while True:
       client, address = socket.accept()
@@ -20,7 +21,9 @@ def worker(socket, worker_num):
       curl_request = buffer_string.split("\r")[0]
       print("Curl Request: ", curl_request)
       
-      port = random.choice(config.ACTIVE_SERVERS)
+      port = random.choice(config.ACTIVE_SERVERS) # Selects a random server from the config file. Can be changed to round robin, weighted, etc
+
+      # Queries the respective server, currently only handles GET requests, but can be further configured to handle more if necessary
       response = requests.get('http://localhost:{}'.format(port))
 
       client.send(response.text.encode('utf-8'))
@@ -29,16 +32,20 @@ def worker(socket, worker_num):
       client.close()
 
 if __name__ == '__main__':
+  # Basic socket setup
   serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  serversocket.bind(('',9086))
+  serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Disables TIME_WAIT state of connected sockets
+  serversocket.bind(('',9085)) # Change port to main server port
   serversocket.listen(5)
+
+  # Start multiple worker processes in parallel
   workers = [multiprocessing.Process(target=worker, args=(serversocket, i,)) for i in range(config.WORKER_NUM)]
 
   for p in workers:
-    p.daemon = True
+    p.daemon = True # This kills all subprocesses when the script ends
     p.start()
 
+  # Keep the program running
   while True:
     try:
       time.sleep(10)
