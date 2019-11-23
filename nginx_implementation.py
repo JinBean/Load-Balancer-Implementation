d9060ea2662d68cc455b527d85fb99ee3d721e38
xubuntu <solsticedante@gmail.com>
2019-11-23 16:51:47 +0800

    added leastTime algo to nginx_implementation.py; add requirements.txt that store pip modules that we require

--- nginx_implementation.py
+++ nginx_implementation.py
@@ -31,7 +31,8 @@ def worker(socket, worker_num, queue):
         print("Curl Request: ", curl_request)
 
         # port = random.choice(config.ACTIVE_SERVERS) # Selects a random server from the config file. Can be changed to round robin, weighted, etc
-        port = roundRobin(queue)
+        # port = roundRobin(queue)
+        port = leastConnections()
 
         # Queries the respective server, currently only handles GET requests, but can be further configured to handle more if necessary
         response = requests.get('http://localhost:{}'.format(port))
@@ -60,10 +61,12 @@ def leastLatency():
         with open("response.txt") as response_file:
             response = response_file.read()
         timing = float(response.split("Host is up (")[1].split("s latency")[0])
-        print(port, timing)
+        print("port: {0}, timing: {1}".format(port, timing))
         if timing < min_timing:
             selected_port = port
             min_timing = timing
+
+    print("selected port: {0}".format(selected_port))
     return selected_port
 
 
@@ -76,12 +79,50 @@ def leastConnections(conn_count):
             selected_port = least_connected
     return selected_port
 
+def leastTime(conn_count):
+    """
+    Cost function for each port is
+    active_conn_coeff*(number of active connections) + latency_coeff*(latency timing in seconds)
+    port with min cost function is returned
+    """
+    active_conn_coeff = 1
+    latency_coeff = 1
+
+    scores = {}
+
+    # fill up active connections
+    for port in config.ACTIVE_SERVERS:
+        print("type(conn_count[port])".format(type(conn_count[port])))
+        scores[port] = conn_count[port] * active_conn_coeff
+
+    # fill up latency times
+    for port in config.ACTIVE_SERVERS:
+        os.system("nmap -p " + str(port) + " localhost > response.txt")
+        with open("response.txt") as response_file:
+            response = response_file.read()
+        scores[port] += float(response.split("Host is up (")[1].split("s latency")[0]) * latency_coeff
+
+    # find min value
+    min_value = sys.maxSize
+    selected_port = None
+    for port in scores.keys():
+        if scores[port] < min_value:
+            min_value = scores[port]
+            selected_port = port
+    print("all scores: {0}".format(scores))
+    print("selected_port: {0}".format(selected_port))
+    return selected_port
+
 
 def randomAssignment():
     return random.choice(config.ACTIVE_SERVERS)
 
 
 if __name__ == '__main__':
+    ports = config.ACTIVE_SERVERS
+    temp_list = [0] * len(ports)
+    active_conn = multiprocessing.Array("i", temp_list)
+
     # Basic socket setup
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Disables TIME_WAIT state of connected sockets
